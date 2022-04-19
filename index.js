6112081e32bcdd9354d45dae2a6385d84b1a4d6d
Joar <101109177+joar1@users.noreply.github.com>
2022-04-19 11:15:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function showTime(){
+   var date = new Date();
+   var hour = date.getHours();
+   var min = date.getMinutes();
+   var sec = date.getSeconds();
+   /* "?" er en shorthand av if-else */
+   hour = (hour < 10) ? "0" + hour : hour;
+   min = (min < 10) ? "0" + min : min;
+   sec = (sec < 10) ? "0" + sec : sec;     
+   var time = hour + ":" + min + ":" + sec + " "; 
+
+   document.getElementById("myClockDisplay").textContent = time;
+   setTimeout(showTime, 1000);
+}
+
+showTime();
